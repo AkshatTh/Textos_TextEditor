6e9e02172e0370178a3d7e26b51ae03134bf68d6
AkshatTh <akshatt.th@gmail.com>
2025-08-09 02:09:01 +0530

    Disables Raw Mode after Exiting Program

--- Textos.c
+++ Textos.c
@@ -1,14 +1,36 @@
+#include <stdlib.h>
 #include <termios.h>
 #include <unistd.h>
-void enableRawMode() {
-  struct termios raw;
-  tcgetattr(STDIN_FILENO, &raw);
-  raw.c_lflag &= ~(ECHO);
-  tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+
+struct termios orig_termios;
+
+void disablerawmode(){
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
 }
-int main() {
-  enableRawMode();
-  char c;
-  while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
-  return 0;
+
+void enablerawmode(){
+
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disablerawmode);
+    
+    tcgetattr(STDIN_FILENO, &orig_termios);
+
+    struct termios raw= orig_termios;
+    
+    raw.c_lflag &= ~(ECHO);
+    
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
+int main(){
+
+    enablerawmode();
+
+    char c;
+
+    while(read(STDIN_FILENO, &c,1)==1 && c!='q');
+
+    
+    return 0;
 }
